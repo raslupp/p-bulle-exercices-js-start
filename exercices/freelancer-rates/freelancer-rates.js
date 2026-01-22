d8e91a2d7268b559d2ae6c91e1d723ea0e9d82b4
Flamur <flamurekaliu@gmail.com>
2026-01-22 14:41:28 +0100

    chore(freelance-rates) : Exercice terminé [15] [DONE]

--- exercices/freelancer-rates/freelancer-rates.js
+++ exercices/freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return Math.floor(budget / (ratePerHour * 8));
 }
 
 /**
@@ -49,5 +49,10 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  const daily = dayRate(ratePerHour);
+  const fullMonths = Math.floor(numDays / 22);
+  const remaining = numDays % 22;
+ 
+  return Math.ceil((fullMonths * 22 * daily) * (1 - discount) + (remaining * daily));
+ 
 }
