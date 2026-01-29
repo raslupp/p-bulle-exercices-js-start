562c8eef5ee79b015228814ad69a9e332ddd98cb
Flamur <flamurekaliu@gmail.com>
2026-01-29 15:56:15 +0100

    chore(annalyns) : Exercice corrigé V2 [5] [DONE]

--- exercices/annalyns-infiltration/annalyns-infiltration.js
+++ exercices/annalyns-infiltration/annalyns-infiltration.js
@@ -53,8 +53,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  if (!archerIsAwake && prisonerIsAwake) return true;
-  else return false;
+  return !archerIsAwake && prisonerIsAwake;
 }
 
 /**
